978298e42ef8904bc29fc4d0b979f9d4392b1782
CSC <CSC@DESKTOP-U23RQH1>
2021-10-23 13:15:45 +0530

    Add project files.

---
+++ snake and ladder game/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace snake_and_ladder_game
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int position = 0;
+            Console.WriteLine("Welcome to snake & ladder game");
+            Console.WriteLine("starting position is " +position);
+
+
+            usercase7 player1 = new usercase7();
+            usercase7 player2 = new usercase7();
+            player1.Die1();
+            player2.Die1();
+            player1.Die2();
+            player2.Die2();
+            player1.Check1();
+            Console.WriteLine("Player1's total move " + player1.Check1() + " ");
+            player2.Check2();
+            Console.WriteLine("Player2's total move " + player2.Check2() + "");
+
+            if (player1.Check1() > player2.Check2())
+            {
+                Console.WriteLine("Player1 is won by " + (player1.Check1() - player2.Check2()) + " moves");
+            }
+            else
+            {
+                Console.WriteLine("Player2 is won by " + (player2.Check2() - player1.Check1()) + " moves");
+            }
+        }
+    }
+}
+
+        
+    
+
---
+++ snake and ladder game/usercase2.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace snake_and_ladder_game
+{
+    class usercase2
+    {
+        public int dieRoll()
+        {
+
+            Random random = new Random();
+            int outcome = random.Next(1, 6);
+            Console.WriteLine("outcome of die1 is " + outcome);
+            return outcome;
+        }
+    }
+}
+    
+
